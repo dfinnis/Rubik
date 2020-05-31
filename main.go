e0fed819322d79fa656f51553c2bb4d974c2f40a
Drew Finnis <drewfinnis@gmail.com>
2020-05-31 14:52:07 +0200

    feature/dumpCube color

--- main.go
+++ main.go
@@ -4,6 +4,22 @@ import (
 	"fmt"
 )
 
+const Reset		= "\x1B[0m"
+const White		= "\x1B[0m"
+const Red		= "\x1B[31m"
+const Green		= "\x1B[32m"
+const Yellow	= "\x1B[33m"
+const Blue		= "\x1B[34m"
+const Orange	= "\x1B[38;2;255;165;0m"
+
+// const WhiteBG	= "\x1B[0m"
+// const RedBG		= "\x1B[31m"
+// const GreenBG	= "\x1B[32m"
+// const YellowBG	= "\x1B[33m"
+// const BlueBG	= "\x1B[34m"
+// const OrangeBG	= "\x1B[48;2;255;165;0m"
+
+
 type face struct {
 	pieces	[3][3]uint8
 }
@@ -30,13 +46,28 @@ func initRubik() *rubik {
 }
 
 func dumpCube(cube *[6]face) {
+	// fmt.Printf(Orange)//////
 	for face := 0; face < 6; face++ {
 		fmt.Printf("Face: %d\n", face)
-		fmt.Printf("%d\n", cube[face])
+		// fmt.Printf("%d\n", cube[face])
 		for y := 0; y < 3; y++ {
 			for x := 0; x < 3; x++ {
-				fmt.Printf("%d%d: %d\n", y, x, cube[face].pieces[y][x])
+				// fmt.Printf("%d ", cube[face].pieces[y][x])
+				if cube[face].pieces[y][x] == 0 {
+					fmt.Printf("1 ")				
+				} else if cube[face].pieces[y][x] == 1 {
+					fmt.Printf("%v1%v ", Red, Reset)
+				} else if cube[face].pieces[y][x] == 2 {
+					fmt.Printf("%v2%v ", Green, Reset)
+				} else if cube[face].pieces[y][x] == 3 {
+					fmt.Printf("%v3%v ", Yellow, Reset)
+				} else if cube[face].pieces[y][x] == 4 {
+					fmt.Printf("%v4%v ", Blue, Reset)
+				} else if cube[face].pieces[y][x] == 5 {
+					fmt.Printf("%v5%v ", Orange, Reset)
+				}
 			}
+			fmt.Printf("\n")/////
 		}
 		fmt.Printf("\n")/////
 	}
