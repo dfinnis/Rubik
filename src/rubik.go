5636f81f2d6ea204bfaa5904bb14945aad065983
Drew Finnis <drewfinnis@gmail.com>
2020-06-24 17:43:04 +0200

    feature/randomMix()

--- src/rubik.go
+++ src/rubik.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	// "reflect"//
+	"math/rand"
+	"time"
 )
 
 type face struct {
@@ -57,16 +59,54 @@ func parseArg() string {
 
 // }
 
+func randomMix() string {
+	var mix string
+	spin := []string{
+		"U",
+		"U'",
+		"U2",
+		"D",
+		"D'",
+		"D2",
+		"R",
+		"R'",
+		"R2",
+		"L",
+		"L'",
+		"L2",
+		"F",
+		"F'",
+		"F2",
+		"B",
+		"B'",
+		"B2",
+	}
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(20) + 20
+	for i := 0; i <= n; i++ {
+		mix += 	spin[rand.Intn(len(spin))]
+		if i != n {
+			mix += " "
+		}
+	}
+	return mix
+}
+
 func RunRubik() {
 	r := initRubik()
-	parseArg()
+	// parseArg()
 	// dumpCube(&r.cube)
 	mix := parseArg()
-	fmt.Printf("mix: %s\n", mix)
+	// mix := randomMix()
+	random := randomMix()
+	fmt.Printf("random: %s\n", random)//
+	fmt.Printf("mix: %s\n", mix)//
 	dumpCube(&r.cube)////
 	spin(mix, r)
+	// spin(random, r)
 	// solution := solve(&r.cube)
 	// printSolution(solution)
+	// spin(solution, r)
 	// rubik.runGraphic()
 	// dumpCube(&r.cube)////
 }
