36ed7ad673d95bf97609905c33e5fcac94b91020
Drew Finnis <drewfinnis@gmail.com>
2020-06-29 21:53:28 +0200

    feature/WIP spinR

--- src/spin.go
+++ src/spin.go
@@ -170,12 +170,95 @@ func spinD2(cube *[6]uint32) {
 	cube[4] |= swap
 }
 
+
+func spinR(cube *[6]uint32) {
+	spinFace(cube, 3)
+	// spin edges
+	swap := cube[2]
+	swap &= 0x00777000
+
+	tmp := cube[5]
+	tmp &= 0x00777000
+	cube[2] &= 0x77000777
+	cube[2] |= tmp
+	
+	tmp0 := cube[4]
+	// tmp1 := cube[4]
+	// tmp2 := cube[4]	
+	tmp0 &= 0x00000070
+	fmt.Printf("tmp0: %032b\n", tmp0)//!!!!
+	tmp0 &= 0x00000070/////////////rotate
+	fmt.Printf("tmp0: %032b\n", tmp0)//!!!!
+	cube[5] &= 0x77000777
+	fmt.Printf("cube[5]: %032b\n", cube[5])//!!!!
+
+	// cube[2] |= tmp
+
+	// tmp = cube[4]
+	// tmp &= 0x00007770
+	// cube[3] &= 0x77770007
+	// cube[3] |= tmp
+
+	// cube[4] &= 0x77770007
+	// cube[4] |= swap
+}
+
+func spinRa(cube *[6]uint32) {
+	spinFaceAnti(cube, 3)
+	// spin edges
+	// swap := cube[1]
+	// swap &= 0x00007770
+
+	// tmp := cube[4]
+	// tmp &= 0x00007770
+	// cube[1] &= 0x77770007
+	// cube[1] |= tmp
+	
+	// tmp = cube[3]
+	// tmp &= 0x00007770
+	// cube[4] &= 0x77770007
+	// cube[4] |= tmp
+
+	// tmp = cube[2]
+	// tmp &= 0x00007770
+	// cube[3] &= 0x77770007
+	// cube[3] |= tmp
+
+	// cube[2] &= 0x77770007
+	// cube[2] |= swap
+}
+
+func spinR2(cube *[6]uint32) {
+	spinFace2(cube, 3)
+	// spin edges
+	// swap := cube[1]
+	// swap &= 0x00007770
+
+	// tmp := cube[3]
+	// tmp &= 0x00007770
+	// cube[1] &= 0x77770007
+	// cube[1] |= tmp
+	
+	// cube[3] &= 0x77770007
+	// cube[3] |= swap
+
+	// swap = cube[2]
+	// swap &= 0x00007770
+
+	// tmp = cube[4]
+	// tmp &= 0x00007770
+	// cube[2] &= 0x77770007
+	// cube[2] |= tmp
+	
+	// cube[4] &= 0x77770007
+	// cube[4] |= swap
+}
+
 func spin(mix string, cube *[6]uint32) {
 	sequence := strings.Fields(mix)
 	fmt.Printf("\nsequence: %v, len: %d\n", sequence, len(sequence)) //
 	for spin := 0; spin < len(sequence); spin++ {
-		fmt.Printf("\nspin: %v\n", spin) //
-		fmt.Printf("\nspin: %v\n", sequence[spin]) //
+		fmt.Printf("\nspin %v: %v\n", spin, sequence[spin]) //
 		if sequence[spin] == "U" {
 			spinU(cube)
 		} else if sequence[spin] == "U'" {
@@ -188,13 +271,12 @@ func spin(mix string, cube *[6]uint32) {
 			spinDa(cube)
 		} else if sequence[spin] == "D2" {
 			spinD2(cube)
-	// 	} else if sequence[spin] == "R" {
-	// 		spinR(&r.cube)
-	// 	} else if sequence[spin] == "R'" {
-	// 		spinRa(&r.cube)
-	// 	} else if sequence[spin] == "R2" {
-	// 		spinR(&r.cube)
-	// 		spinR(&r.cube)
+		} else if sequence[spin] == "R" {
+			spinR(&r.cube)
+		} else if sequence[spin] == "R'" {
+			spinRa(&r.cube)
+		} else if sequence[spin] == "R2" {
+			spinR2(&r.cube)
 	// 	} else if sequence[spin] == "L" {
 	// 		spinL(&r.cube)
 	// 	} else if sequence[spin] == "L'" {
