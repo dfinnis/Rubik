32ad2ccf5833e67162e1cfbf789d2c8c5774f79c
Drew Finnis <drewfinnis@gmail.com>
2020-07-09 14:32:32 +0200

    feature/1 random test

--- test.sh
+++ test.sh
@@ -191,21 +191,108 @@ else
 fi
 ((count+=1))
 
+#### -- Random 1 -- ####
+cmd="./Rubik -r"
+output=$(eval "$cmd")
+incorrect=$(echo "$output" | tail -n 7 | head -n 1 )
+time=$(echo "$output" | tail -n 1)
+prefix=$(echo "$time" | rev | cut -c-1-2 | rev | cut -c-1-1)
+if [ "$prefix" = "m" ]
+then
+	time_cut=$(echo "$time" | rev | cut -c3-42 | rev)
+	time_cut=$(echo "scale = 9; ($time_cut / 1000)" | bc)
+	time_cumulative=$(echo "scale = 9; $time_cumulative + $time_cut" | bc)
+	time_up=$(echo "scale = 0; $time_cut * 1000000000" | bc | cut -d "." -f 1)
+	worst_up=$(echo "scale = 0; $time_worst * 1000000000" | bc | cut -d "." -f 1)
+	best_up=$(echo "scale = 0; $time_best * 1000000000" | bc | cut -d "." -f 1)
+	if [ "$time_up" -gt "$worst_up" ]
+	then
+		time_worst=$time_cut
+	fi
+	if [ "$time_up" -lt "$best_up" ]
+	then
+		time_best=$time_cut
+	fi
+elif [ "$prefix" = "µ" ]
+then
+	time_cut=$(echo "$time" | rev | cut -c3-42 | rev)
+	time_cut=$(echo "scale = 9; ($time_cut / 1000000)" | bc)
+	time_cumulative=$(echo "scale = 9; $time_cumulative + $time_cut" | bc)
+	time_up=$(echo "scale = 0; $time_cut * 1000000000" | bc | cut -d "." -f 1)
+	worst_up=$(echo "scale = 0; $time_worst * 1000000000" | bc | cut -d "." -f 1)
+	best_up=$(echo "scale = 0; $time_best * 1000000000" | bc | cut -d "." -f 1)
+	if [ "$time_up" -gt "$worst_up" ]
+	then
+		time_worst=$time_cut
+	fi
+	if [ "$time_up" -lt "$best_up" ]
+	then
+		time_best=$time_cut
+	fi
+elif [ "$prefix" = "n" ]
+then
+	time_cut=$(echo "$time" | rev | cut -c3-42 | rev)
+	time_cut=$(echo "scale = 9; ($time_cut / 1000000000)" | bc)
+	time_cumulative=$(echo "scale = 9; $time_cumulative + $time_cut" | bc)
+	time_up=$(echo "scale = 0; $time_cut * 1000000000" | bc | cut -d "." -f 1)
+	worst_up=$(echo "scale = 0; $time_worst * 1000000000" | bc | cut -d "." -f 1)
+	best_up=$(echo "scale = 0; $time_best * 1000000000" | bc | cut -d "." -f 1)
+	if [ "$time_up" -gt "$worst_up" ]
+	then
+		time_worst=$time_cut
+	fi
+	if [ "$time_up" -lt "$best_up" ]
+	then
+		time_best=$time_cut
+	fi
+else
+	time_cut=$(echo "$time" | rev | cut -c2-42 | rev)
+	time_cumulative=$(echo "scale = 9; $time_cumulative + $time_cut" | bc)
+	time_up=$(echo "scale = 0; $time_cut * 1000000000" | bc | cut -d "." -f 1)
+	worst_up=$(echo "scale = 0; $time_worst * 1000000000" | bc | cut -d "." -f 1)
+	best_up=$(echo "scale = 0; $time_best * 1000000000" | bc | cut -d "." -f 1)
+	if [ "$time_up" -gt "$worst_up" ]
+	then
+		time_worst=$time_cut
+	fi
+	if [ "$time_up" -lt "$best_up" ]
+	then
+		time_best=$time_cut
+	fi
+fi
+htm=$(echo "$output" | tail -n 4 | head -n 1 | wc -w)
+if [ "$htm" -gt "$htm_worst" ]
+then
+	htm_worst=$htm
+fi
+if [ "$htm" -lt "$htm_best" ]
+then
+	htm_best=$htm
+fi
+htm_cumulative=$(echo "scale = 9; $htm_cumulative + $htm" | bc)
+if [ "$incorrect" == "Error: Solution incorrect :(" ]
+then
+	echo "\x1b[31mRandom 1:\t\tERROR\t  $htm\t$time\x1b[0m"
+else
+	echo "\x1b[32mRandom 1:\t\tOK\t  $htm\t$time\x1b[0m"
+	((solved+=1))
+fi
+((count+=1))
+
 
 #### -- END -- ####
-rm Rubik
 # echo "\n\n\x1b[1mAll Rubik tests finished\x1b[0m" ##\nTotal runtime $SECONDS seconds"
+rm Rubik
 echo
 if [ "$solved" == "$count" ]
 then
-	echo "\n\x1b[32mPassed $solved of $count total tests\x1b[0m\n"
+	echo "\nSolved\x1b[32m $solved of $count \x1b[0mtotal cubes\n"
 elif [ "$solved" == "0" ]
 then
-	echo "\n\x1b[31mPassed $solved of $count total tests\x1b[0m\n"	
+	echo "\nSolved\x1b[31m $solved of $count \x1b[0mtotal cubes\n"	
 else
-	echo "\n\x1b[33mPassed $solved of $count total tests\x1b[0m\n"
+	echo "\nSolved\x1b[33m $solved of $count \x1b[0mtotal cubes\n"
 fi
-
 
 echo "\x1b[1mHalf-turn metric\x1b[0m"
 mean_int=$(echo "scale = 0; $htm_cumulative / $count" | bc)
@@ -232,24 +319,39 @@ fi
 echo "\n\x1b[1mSolve time\x1b[0m"
 time_mean=$(echo "scale = 9; $time_cumulative / $count" | bc)
 best_cut=$(echo $time_best | cut -d "." -f 1)
-if [ "$best_cut" -lt 23 ]
+if [ "$best_cut" == "" ]
 then
 	echo "\x1b[32mBest:\t      $time_best\x1b[32ms\x1b[0m"
 else
-	echo "\x1b[31mBest:\t      $time_best\x1b[31ms\x1b[0m"
+	if [ "$best_cut" -lt 23 ]
+	then
+		echo "\x1b[32mBest:\t      $time_best\x1b[32ms\x1b[0m"
+	else
+		echo "\x1b[31mBest:\t      $time_best\x1b[31ms\x1b[0m"
+	fi
 fi
 worst_cut=$(echo $time_worst | cut -d "." -f 1)
-if [ "$worst_cut" -lt 23 ]
+if [ "$worst_cut" == "" ]
 then
 	echo "\x1b[32mWorst:\t      $time_worst\x1b[32ms\x1b[0m"
 else
-	echo "\x1b[31mWorst:\t      $time_worst\x1b[31ms\x1b[0m"
+	if [ "$worst_cut" -lt 23 ]
+	then
+		echo "\x1b[32mWorst:\t      $time_worst\x1b[32ms\x1b[0m"
+	else
+		echo "\x1b[31mWorst:\t      $time_worst\x1b[31ms\x1b[0m"
+	fi
 fi
 mean_cut=$(echo $time_mean | cut -d "." -f 1)
-if [ "$mean_cut" -lt 23 ]
+if [ "$mean_cut" == "" ]
 then
 	echo "\x1b[32mMean:\t      $time_mean\x1b[32ms\x1b[0m"
 else
-	echo "\x1b[31mMean:\t      $time_mean\x1b[31ms\x1b[0m"
+if [ "$mean_cut" -lt 23 ]
+	then
+		echo "\x1b[32mMean:\t      $time_mean\x1b[32ms\x1b[0m"
+	else
+		echo "\x1b[31mMean:\t      $time_mean\x1b[31ms\x1b[0m"
+	fi
 fi
 echo
