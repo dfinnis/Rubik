a08bd65f07643958133faa57e6d81fc972446694
Drew Finnis <drewfinnis@gmail.com>
2020-07-23 18:47:29 +0200

    feature/WIP heuristic, comment debug, fix heuristic output

--- src/solve.go
+++ src/solve.go
@@ -7,23 +7,23 @@ import (
 // 17 moves max
 func G4heuristic(cube *[6]uint32) uint8 {
 	var correct uint8
-	dumpCube(cube)/////
+	// dumpCube(cube)/////
 	var face uint32
 	for face = 0; face < 6; face++ {
-		fmt.Printf("face: %v\n", face)///
+		// fmt.Printf("face: %v\n", face)///
 		var cubie uint32
 		var mask uint32 = 0x10000000
 		for cubie = 0x70000000; cubie > 0; cubie /= 16 {
-			fmt.Printf("cubie: %x\n", cubie)//
+			// fmt.Printf("cubie: %x\n", cubie)//
 			if cube[face]&cubie == mask * face {
-				fmt.Printf("CORRECT!!!!!!!!!!!\n")//
+				// fmt.Printf("CORRECT!!!!!!!!!!!\n")//
 				correct++
 			}
 			mask /= 16
-			fmt.Printf("mask: %x\n", mask)//
+			// fmt.Printf("mask: %x\n", mask)//
 		}
 	}
-	return correct
+	return 48 - correct
 }
 
 func solve(cube *[6]uint32) string {
