7c518a5d4ee7a0eb713faf729546fc3296fc3aeb
Drew Finnis <drewfinnis@gmail.com>
2020-07-31 14:17:31 +0200

    feature/WIP idaStar, seperate solveHeuristic.go file

--- src/solve.go
+++ src/solve.go
@@ -11,99 +11,6 @@ func newNode(newCube *[6]uint32, move string) *rubik {
 	}
 }
 
-func oppositeFace(face uint32) uint32 {
-	if face == 0 {
-		return 5
-	} else if face == 1 {
-		return 3
-	} else if face == 2 {
-		return 4
-	} else if face == 3 {
-		return 1
-	} else if face == 4 {
-		return 2
-	} else {
-		return 0
-	}
-}
-
-func heuristicG0(cube *[6]uint32) uint8 {
-	return 42
-}
-
-func heuristicG1(cube *[6]uint32) uint8 {
-	return 42
-}
-
-// 15 moves max
-func heuristicG2(cube *[6]uint32) uint8 {
-	var color uint8
-	var parity uint8
-	var face uint32
-	for face = 0; face < 6; face++ {
-		var cubie uint32
-		var mask uint32 = 0x10000000
-		for cubie = 0x70000000; cubie > 0; cubie /= 16 {
-			if cube[face]&cubie == mask * face || cube[face]&cubie == mask * oppositeFace(face) {
-				color++
-			}
-			mask /= 16
-		}
-	}
-	// fmt.Printf("color: %v\n", color)//
-	for _, face := range [4]uint8{0, 2, 4, 5} {
-		// fmt.Printf("face: %v\n", face)//
-		// fmt.Printf("cube[face]&0x70000000: %x\n", cube[face]&0x70000000)//
-		// fmt.Printf("cube[face]&0x70000000 / uint32(math.Pow(16, 6): %x\n", cube[face]&0x70000000 / uint32(math.Pow(16, 6)))//
-		// fmt.Printf("cube[face]&0x70000000  >> 6: %x\n", cube[face]&0x70000000 >> 24)//
-
-		// fmt.Printf("cube[face]&0x70: %x\n", cube[face]&0x70)//
-		if cube[face]&0x70000000 >> (6 * 4) == cube[face]&0x70 {
-			parity++
-		}
-		if cube[face]&0x700000 >> (2 * 4) == cube[face]&0x7000 {
-			parity++
-		}
-	}
-	// fmt.Printf("parity: %v\n", parity)//
-	if parity == 0 {
-		parity = 8
-	}
-	if parity != 8 {
-		parity = 0
-	}
-	return (56 - (color + parity)) / 4
-}
-
-// 17 moves max
-func heuristicG3(cube *[6]uint32) uint8 {
-	var correct uint8
-	var face uint32
-	for face = 0; face < 6; face++ {
-		var cubie uint32
-		var mask uint32 = 0x10000000
-		for cubie = 0x70000000; cubie > 0; cubie /= 16 {
-			if cube[face]&cubie == mask * face {
-				correct++
-			}
-			mask /= 16
-		}
-	}
-	return (48 - correct) / 4
-}
-
-func heuristic(cube *[6]uint32, subgroup uint8) uint8 {
-	if subgroup == 0 {
-		return heuristicG0(cube)
-	} else if subgroup == 1 {
-		return heuristicG1(cube)
-	} else if subgroup == 2 {
-		return heuristicG2(cube)
-	} else { // if subgroup == 3 {
-		return heuristicG3(cube)
-	}	
-}
-
 func inPath(node *rubik, path []rubik) bool {
 	for i := range path {
 		if node.cube == path[i].cube {
@@ -135,11 +42,9 @@ func idaStar(r *rubik) string {
 	path = append(path, *r)
 	for {
 		cost, solution := search(path, 0, bound, subgroup)
-		// if t = FOUND then return (path, bound)
 		if cost == 255 {
 			// fmt.Printf("***************	END	********************\n")//
 			// dumpPath(path)//
-			// return "Found" /// replace with solution!!
 			return solution
 		}
 		// if t = ∞ then return NOT_FOUND
@@ -148,60 +53,6 @@ func idaStar(r *rubik) string {
 	}
 	// return "Error"//
 	// dumpCube(&path[0].cube)//
-}
-
-func listMoves(node *rubik, subgroup uint8) []string {
-	// fmt.Printf("subgroup: %v\n", subgroup)//
-	moves := []string{
-		"U",
-		"D",
-		"R",
-		"L",
-		"F",
-		"B",
-	}
-	if subgroup == 1 {
-		moves = []string{
-			"U2",
-			"D2",
-			"R",
-			"L",
-			"F",
-			"B",
-		}
-		// fmt.Printf("Group 1!!!!!!!!!!!!\n")////
-	} else if subgroup == 2 {
-		moves = []string{
-			"U2",
-			"D2",
-			"R",
-			"L",
-			"F2",
-			"B2",
-		}
-		// fmt.Printf("Group 2!!!!!!!!!!!!\n")////
-	} else if subgroup == 3 {
-		moves = []string{
-			"U2",
-			"D2",
-			"R2",
-			"L2",
-			"F2",
-			"B2",
-		}
-		// fmt.Printf("Group 3!!!!!!!!!!!!\n")////
-	}
-	// fmt.Printf("move: %v\n", node.move)//
-	if node.move != "" {
-		for i, move := range moves {
-			if move == node.move {
-				moves = append(moves[:i], moves[i+1:]...)
-				break
-			}
-		}
-	}
-	// fmt.Printf("moves: %v\n", moves)//
-	return moves
 }
 
 func search(path []rubik, g uint8, bound uint8, subgroup uint8) (uint8, string) {
---
+++ src/solveHeuristic.go
@@ -0,0 +1,145 @@
+package rubik
+
+func listMoves(node *rubik, subgroup uint8) []string {
+	moves := []string{}
+	if subgroup == 0 {
+		moves = []string{
+			"U",
+			"D",
+			"R",
+			"L",
+			"F",
+			"B",
+		}
+	} else if subgroup == 1 {
+		moves = []string{
+			"U2",
+			"D2",
+			"R",
+			"L",
+			"F",
+			"B",
+		}
+	} else if subgroup == 2 {
+		moves = []string{
+			"U2",
+			"D2",
+			"R",
+			"L",
+			"F2",
+			"B2",
+		}
+	} else { //if subgroup == 3 {
+		moves = []string{
+			"U2",
+			"D2",
+			"R2",
+			"L2",
+			"F2",
+			"B2",
+		}
+	}
+	if node.move != "" {
+		for i, move := range moves {
+			if move == node.move {
+				moves = append(moves[:i], moves[i+1:]...)
+				break
+			}
+		}
+	}
+	// fmt.Printf("moves: %v\n", moves)//
+	return moves
+}
+
+func oppositeFace(face uint32) uint32 {
+	if face == 0 {
+		return 5
+	} else if face == 1 {
+		return 3
+	} else if face == 2 {
+		return 4
+	} else if face == 3 {
+		return 1
+	} else if face == 4 {
+		return 2
+	} else {
+		return 0
+	}
+}
+
+func heuristicG0(cube *[6]uint32) uint8 {
+	return 42
+}
+
+func heuristicG1(cube *[6]uint32) uint8 {
+	return 42
+}
+
+// 15 moves max
+func heuristicG2(cube *[6]uint32) uint8 {
+	var color uint8
+	var parity uint8
+	var face uint32
+	for face = 0; face < 6; face++ {
+		var cubie uint32
+		var mask uint32 = 0x10000000
+		for cubie = 0x70000000; cubie > 0; cubie /= 16 {
+			if cube[face]&cubie == mask * face || cube[face]&cubie == mask * oppositeFace(face) {
+				color++
+			}
+			mask /= 16
+		}
+	}
+	// fmt.Printf("color: %v\n", color)//
+	for _, face := range [4]uint8{0, 2, 4, 5} {
+		// fmt.Printf("face: %v\n", face)//
+		// fmt.Printf("cube[face]&0x70000000: %x\n", cube[face]&0x70000000)//
+		// fmt.Printf("cube[face]&0x70000000 / uint32(math.Pow(16, 6): %x\n", cube[face]&0x70000000 / uint32(math.Pow(16, 6)))//
+		// fmt.Printf("cube[face]&0x70000000  >> 6: %x\n", cube[face]&0x70000000 >> 24)//
+
+		// fmt.Printf("cube[face]&0x70: %x\n", cube[face]&0x70)//
+		if cube[face]&0x70000000 >> (6 * 4) == cube[face]&0x70 {
+			parity++
+		}
+		if cube[face]&0x700000 >> (2 * 4) == cube[face]&0x7000 {
+			parity++
+		}
+	}
+	// fmt.Printf("parity: %v\n", parity)//
+	if parity == 0 {
+		parity = 8
+	}
+	if parity != 8 {
+		parity = 0
+	}
+	return (56 - (color + parity)) / 4
+}
+
+// 17 moves max
+func heuristicG3(cube *[6]uint32) uint8 {
+	var correct uint8
+	var face uint32
+	for face = 0; face < 6; face++ {
+		var cubie uint32
+		var mask uint32 = 0x10000000
+		for cubie = 0x70000000; cubie > 0; cubie /= 16 {
+			if cube[face]&cubie == mask * face {
+				correct++
+			}
+			mask /= 16
+		}
+	}
+	return (48 - correct) / 4
+}
+
+func heuristic(cube *[6]uint32, subgroup uint8) uint8 {
+	if subgroup == 0 {
+		return heuristicG0(cube)
+	} else if subgroup == 1 {
+		return heuristicG1(cube)
+	} else if subgroup == 2 {
+		return heuristicG2(cube)
+	} else { // if subgroup == 3 {
+		return heuristicG3(cube)
+	}	
+}
