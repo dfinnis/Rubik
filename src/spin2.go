f745024bdbd4f1bfed6e688c803525d991ecbe6d
Drew Finnis <drewfinnis@gmail.com>
2020-08-13 20:27:38 +0200

    feature/spin ALL

--- src/spin2.go
+++ src/spin2.go
@@ -91,6 +91,56 @@ func spinCepoF(cube *cepo) {
 	cube.cO[cube.cP[6]] = modulo3(cube.cO[cube.cP[6]] - 1)
 }
 
+func spinCepoB(cube *cepo) {
+	// corner permutation
+	tmp := cube.cP[7]
+	cube.cP[7] = cube.cP[2]
+	cube.cP[2] = cube.cP[5]
+	cube.cP[5] = cube.cP[0]
+	cube.cP[0] = tmp
+	// edge permutation
+	tmp = cube.eP[7]
+	cube.eP[7] = cube.eP[10]
+	cube.eP[10] = cube.eP[4]
+	cube.eP[4] = cube.eP[11]
+	cube.eP[11] = tmp
+	// corner orientation
+	cube.cO[cube.cP[0]] = modulo3(cube.cO[cube.cP[0]] + 1)
+	cube.cO[cube.cP[2]] = modulo3(cube.cO[cube.cP[2]] + 1)
+	cube.cO[cube.cP[5]] = modulo3(cube.cO[cube.cP[5]] - 1)
+	cube.cO[cube.cP[7]] = modulo3(cube.cO[cube.cP[7]] - 1)
+}
+
+func spinCepoL(cube *cepo) {
+	// corner permutation
+	tmp := cube.cP[0]
+	cube.cP[0] = cube.cP[5]
+	cube.cP[5] = cube.cP[1]
+	cube.cP[1] = cube.cP[4]
+	cube.cP[4] = tmp
+	// edge permutation
+	tmp = cube.eP[4]
+	cube.eP[4] = cube.eP[1]
+	cube.eP[1] = cube.eP[5]
+	cube.eP[5] = cube.eP[0]
+	cube.eP[0] = tmp
+}
+
+func spinCepoR(cube *cepo) {
+	// corner permutation
+	tmp := cube.cP[3]
+	cube.cP[3] = cube.cP[6]
+	cube.cP[6] = cube.cP[2]
+	cube.cP[2] = cube.cP[7]
+	cube.cP[7] = tmp
+	// edge permutation
+	tmp = cube.eP[6]
+	cube.eP[6] = cube.eP[2]
+	cube.eP[2] = cube.eP[7]
+	cube.eP[7] = cube.eP[3]
+	cube.eP[3] = tmp
+}
+
 func spinCepo(mix string, cube *cepo) {
 	sequence := strings.Fields(mix)
 	// fmt.Printf("\nsequence: %v, len: %d\n", sequence, len(sequence))	//	debug tool
@@ -114,18 +164,24 @@ func spinCepo(mix string, cube *cepo) {
 		} else if sequence[spin] == "D2" {
 			spinCepoD(cube)
 			spinCepoD(cube)
-		// } else if sequence[spin] == "R" {
-		// 	spinCepoR(cepo)
-		// } else if sequence[spin] == "R'" {
-		// 	spinCepoRa(cepo)
-		// } else if sequence[spin] == "R2" {
-		// 	spinCepoR2(cepo)
-		// } else if sequence[spin] == "L" {
-		// 	spinCepoL(cepo)
-		// } else if sequence[spin] == "L'" {
-		// 	spinCepoLa(cepo)
-		// } else if sequence[spin] == "L2" {
-		// 	spinCepoL2(cepo)
+		} else if sequence[spin] == "R" {
+			spinCepoR(cube)
+		} else if sequence[spin] == "R'" {
+			spinCepoR(cube)
+			spinCepoR(cube)
+			spinCepoR(cube)
+		} else if sequence[spin] == "R2" {
+			spinCepoR(cube)
+			spinCepoR(cube)
+		} else if sequence[spin] == "L" {
+			spinCepoL(cube)
+		} else if sequence[spin] == "L'" {
+			spinCepoL(cube)
+			spinCepoL(cube)
+			spinCepoL(cube)
+		} else if sequence[spin] == "L2" {
+			spinCepoL(cube)
+			spinCepoL(cube)
 		} else if sequence[spin] == "F" {
 			spinCepoF(cube)
 		} else if sequence[spin] == "F'" {
@@ -135,12 +191,15 @@ func spinCepo(mix string, cube *cepo) {
 		} else if sequence[spin] == "F2" {
 			spinCepoF(cube)
 			spinCepoF(cube)
-		// } else if sequence[spin] == "B" {
-		// 	spinCepoB(cepo)
-		// } else if sequence[spin] == "B'" {
-		// 	spinCepoBa(cepo)
-		// } else if sequence[spin] == "B2" {
-		// 	spinCepoB2(cepo)
+		} else if sequence[spin] == "B" {
+			spinCepoB(cube)
+		} else if sequence[spin] == "B'" {
+			spinCepoB(cube)
+			spinCepoB(cube)
+			spinCepoB(cube)
+		} else if sequence[spin] == "B2" {
+			spinCepoB(cube)
+			spinCepoB(cube)
 		} else {
 			errorExit("bad input")
 		}
