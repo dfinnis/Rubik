747def7430e9ae160402714a1f5ef9cdb4ad2e74
Drew Finnis <drewfinnis@gmail.com>
2020-08-27 19:18:04 +0200

    refacto/rm listAllMoves func

--- src/moves.go
+++ src/moves.go
@@ -1,9 +1,5 @@
 package rubik
 
-// import (
-// 	"fmt"//
-// )
-
 func listMoves(cube *cepo, subgroup int8) []string {
 	moves := []string{}
 	if subgroup == 0 {
@@ -82,72 +78,5 @@ func listMoves(cube *cepo, subgroup int8) []string {
 		}
 		moves = dry
 	}
-	// fmt.Printf("moves: %v\n", moves)//
-	return moves
-}
-
-func listAllMoves(cube *cepo) []string {
-	moves := []string{
-			"U",
-			"U'",
-			"U2",
-			"D",
-			"D'",
-			"D2",
-			"R",
-			"R'",
-			"R2",
-			"L",
-			"L'",
-			"L2",
-			"F",
-			"F'",
-			"F2",
-			"B",
-			"B'",
-			"B2",
-	}
-	// dry := moves
-	// // // for i := 0; i < n; i++ {
-	// // // 	move := dry[rand.Intn(len(dry))]
-	// // // 	mix += move
-	// if cube.move == "U" || cube.move == "U'" || cube.move == "U2" {
-	// 	if cube.move2 == "D" || cube.move2 == "D'" || cube.move2 == "D2" {
-	// 		dry = moves[3:]
-	// 	} else {
-	// 		dry = moves[6:]
-	// 	}
-// 	} else if move == "D" || move == "D'" || move == "D2" {
-// 		if stringInSlice("U", dry) {
-// 			dry = append([]string{}, spin[:3]...)
-// 			dry = append(dry, spin[6:]...)
-// 		} else {
-// 			dry = spin[6:]
-// 		}
-// 	} else if move == "R" || move == "R'" || move == "R2" {
-// 		dry = append([]string{}, spin[:6]...)
-// 		if stringInSlice("L", dry) {
-// 			dry = append(dry, spin[9:]...)
-// 		} else {
-// 			dry = append(dry, spin[12:]...)
-// 		}
-// 	} else if move == "L" || move == "L'" || move == "L2" {
-// 		if stringInSlice("R", dry) {
-// 			dry = append([]string{}, spin[:9]...)
-// 		} else {
-// 			dry = append([]string{}, spin[:6]...)
-// 		}
-// 		dry = append(dry, spin[12:]...)
-// 	} else if move == "F" || move == "F'" || move == "F2" {
-// 		dry = append([]string{}, spin[:12]...)
-// 		if stringInSlice("B", dry) {
-// 			dry = append(dry, spin[15:]...)
-// 		}
-// 	} else if move == "B" || move == "B'" || move == "B2" {
-// 		if stringInSlice("F", dry) {
-// 			dry = spin[:15]
-// 		} else {
-// 			dry = spin[:12]
-	// }
 	return moves
 }
