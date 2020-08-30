09fda34d3c8190962e9013fc34147eda9c59b54a
Drew Finnis <drewfinnis@gmail.com>
2020-08-30 18:37:48 +0200

    feature/write and read tables/G3 file, tableG3.go cleaned

--- src/tableG3.go
+++ src/tableG3.go
@@ -2,31 +2,8 @@ package rubik
 
 import (
 	"fmt"
-	// "math"
+	"os"
 )
-
-// func ePindexConverter(cube *cepo) [3]int {
-// 	// fmt.Printf("\nOH HIII!\n")
-// 	// cube := initCube()
-// 	// ePindex := 1
-// 	var sliceIndex [3]int
-// 	for slice := 0; slice < 3; slice++ {
-// 		// var sliceIndex int
-// 		for i := slice * 4; i < slice * 4 + 3; i++ {
-// 			// fmt.Printf("cube.eP[i] : %v\n", cube.eP[i])//
-// 			// sliceIndex += (int(cube.eP[i]) - slice * 4) * int(math.Pow(2, float64(2 - i + slice * 4)))
-// 			sliceIndex[slice] += (int(cube.eP[i]) - slice * 4) * int(math.Pow(2, float64(2 - i + slice * 4)))
-// 			// fmt.Printf("math.Pow(2, float64(3-i)): %v\n", math.Pow(2, float64(2-i + slice * 4)))//
-// 			// fmt.Printf("index: %v\n", index)//
-	
-// 		}
-// 		// ePindex *= sliceIndex
-// 		// fmt.Printf("index slice: %v\n\n", index)//
-// 	}
-// 	// fmt.Printf("index done: %v\n", index)//
-// 	// return ePindex
-// 	return sliceIndex
-// }
 
 func ePindexSlice(cube *cepo, slice uint8) uint8 {
 	slice4 := int8(slice * 4)
@@ -119,13 +96,10 @@ func ePindexConverter(cube *cepo) [3]uint8 {
 	for slice = 0; slice < 3; slice++ {
 		sliceIndex[slice] = ePindexSlice(cube, slice)
 	}
-	// fmt.Printf("cube.eP: %v\n", cube.eP)//
-	// fmt.Printf("sliceIndex: %v\n", sliceIndex)//
 	return sliceIndex
 }
 
 func cPtableIndex(tables *tables) {
-	// var cPtableIndex [40320]uint8
 	var initial []cepo
 	var parents []cepo
 	var converted uint8 = 1
@@ -147,8 +121,6 @@ func cPtableIndex(tables *tables) {
 		}
 		parents = children
 	}
-	// fmt.Printf("count: %v\n", count)//
-	// return cPtableIndex
 }
 
 
@@ -157,55 +129,72 @@ func tableG3(tables *tables) {
 	var parents []cepo
 	parents = append(parents, *initCube())
 	var depth uint8
-	var cumulative int//
-	for depth < 15  {// 15
+	// var cumulative int//
+	for depth < 15  {
 		depth++
-		fmt.Printf("len(parents): %v\n", len(parents))//
-		var count int//
+		// var count int//
 		var children []cepo
 		for _, parent := range parents {
 			for _, move := range listMoves(&parent, 3) {
-				// fmt.Printf("\nmove %v: %v\n", i, move)//
 				child := newNode(&parent, move)
 				spin(move, child)
-				// dumpCube(child)//
-
 				idxCP := tables.G3cPindex[cP2index(child)]
 				idxEP := ePindexConverter(child)
-
-				if tables.G3[idxCP][idxEP[0]][idxEP[1]][idxEP[2]] == 0 /*&& !(idxCP == 0 && idxEP[0] == 0 && idxEP[1] == 0 && idxEP[2] == 0)*/ {
+				if tables.G3[idxCP][idxEP[0]][idxEP[1]][idxEP[2]] == 0 {
 					tables.G3[idxCP][idxEP[0]][idxEP[1]][idxEP[2]] = depth
-					count++//
-					cumulative++//
+					// count++//
+					// cumulative++//
 					children = append(children, *child)
 				}
-				// children = append(children, *child)
 			}
 		}
 		parents = children
 		fmt.Printf(".")
-		fmt.Printf("depth: %v\n", depth)//
-		fmt.Printf("count: %v\n", count)//
-		fmt.Printf("cumulative: %v\n\n", cumulative)//
-		// // fmt.Printf("len(parents): %v\n", len(parents))//
-		// // fmt.Printf("tables.G3[0][0]: %v\n\n", tables.G3[0][0])//
-		// // fmt.Printf("tables.G3[0][1]: %v\n\n", tables.G3[0][1])//
-		// // fmt.Printf("tables.G3[0][2]: %v\n\n", tables.G3[0][2])//
-		// // fmt.Printf("tables.G3[40319][69]: %v\n\n", tables.G3[40319][69])//
+		// fmt.Printf("depth: %v\n", depth)//
+		// fmt.Printf("count: %v\n", count)//
+		// fmt.Printf("cumulative: %v\n\n", cumulative)//
 	}
 }
 
-
-
 func makeTableG3(tables *tables) {
-	fmt.Printf("\nGenerating pruning table for G3")
 	cPtableIndex(tables)
-	tableG3(tables)
-	// cube := initCube()
-	// ePindexConverter(cube)
-	// spin("U2 D2 R2", cube)//
-	// // cPindex := cP2index(cube)//
-	// fmt.Printf("\ncPtableIndex[cPindex]: %v\n", cPtableIndex[cP2index(cube)])//
-	// fmt.Printf("\nePindexConverter(cube): %v\n", ePindexConverter(cube))//
-	// fmt.Printf("cPtableIndex: %v\n", cPtableIndex)//
+	if _, err := os.Stat("tables/G3.txt"); os.IsNotExist(err) {
+		tableG3(tables)
+		file := createFile("tables/G3.txt")
+		defer file.Close()
+		for cPidx := 0; cPidx < 96; cPidx++ {
+			for ePidx0 := 0; ePidx0 < 24; ePidx0++ {
+				for ePidx1 := 0; ePidx1 < 24; ePidx1++ {
+					for ePidx2 := 0; ePidx2 < 24; ePidx2++ {
+						_, err = file.WriteString(fmt.Sprintf("%x", tables.G3[cPidx][ePidx0][ePidx1][ePidx2]))
+						if err != nil {
+							errorExit("failed to write to file")
+						}
+					}
+				}
+			}
+		}
+	} else {
+		file := readFile("tables/G3.txt")
+		cPidx := 0
+		ePidx0 := 0
+		ePidx1 := 0
+		ePidx2 := 0
+		for _, depth := range file {
+			tables.G3[cPidx][ePidx0][ePidx1][ePidx2] = readHex(depth)
+			ePidx2++
+			if ePidx2 >= 24 {
+				ePidx2 = 0
+				ePidx1++
+				if ePidx1 >= 24 {
+					ePidx1 = 0
+					ePidx0++
+					if ePidx0 >= 24 {
+						ePidx0 = 0
+						cPidx++
+					}
+				}
+			}
+		}
+	}
 }
